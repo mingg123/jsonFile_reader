ecb8491b8bf607fd9f3d92dbf23e5b4572c70fe5
minjikim7983 <minjikim7983@nexon.co.kr>
2022-07-08 19:25:24 +0900

    임의로 만든 express 서버

---
+++ staticPathTest/app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+// var jsonRoute = require('./Localization/ko-KR/ItemData');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use('/Localization/ko-KR/ItemData', express.static('Localization/ko-KR/ItemData'));
+app.use('/Localization/en-US/ItemData', express.static('Localization/en-US/ItemData'));
+app.use('/Localization/ja-JR/ItemData', express.static('Localization/ja-JR/ItemData'));
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
